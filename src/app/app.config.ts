7370d44ca990543a2f3d0c47dea325c501989469
DesireeCost4 <desireec.dev@gmail.com>
2024-03-16 11:27:06 -0300

    Estrutura Home Page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgModule } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 import { routes } from './app.routes';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [
+    HttpClientModule
+  ]
+})
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
+
+
+
