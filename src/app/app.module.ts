c2184c8b9b76095365e22aad5bd3142148fe3363
Vitoria Poncio <vitoria.poncio@gmail.com>
2024-03-26 21:51:32 -0300

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
+const routes: Routes = [
+];
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     AppRoutingModule,
-    HttpClientModule
-  ]
-})
-export class AppModule { }
-
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-
-
-const routes: Routes = [
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppModule { }
