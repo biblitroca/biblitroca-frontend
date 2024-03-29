902e74f7d598d65e8816c051b4f0358280078c33
DesireeCost4 <desireec.dev@gmail.com>
2024-03-29 15:41:30 -0300

    cadastro de users

--- src/app/components/create-account/create-account.component.ts
+++ src/app/components/create-account/create-account.component.ts
@@ -3,25 +3,52 @@ import { ContinueWithComponent } from '../continue-with/continue-with.component'
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { FooterCardComponent } from '../footer-card/footer-card.component';
 import { CommonModule } from '@angular/common';
-import { ApiService } from '../../api.service';
+import { CreateAccountService } from './create-account.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [ContinueWithComponent, NavBarComponent, FooterCardComponent, RouterModule],
+  imports: [ContinueWithComponent, NavBarComponent, FooterCardComponent, FormsModule,
+    RouterModule, CommonModule, CreateAccountComponent, ReactiveFormsModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.css'
 })
-export class CreateAccountComponent{
+export class CreateAccountComponent implements OnInit {
 
-  constructor(private apiService: ApiService) {}
+  userForm: FormGroup;
 
-  // getResponse(){
-  //   this.apiService.getUsers().subscribe(response => {
-  //     console.log(response);
-  //   });
-  //   throw new Error('deu erro');
-  // }
+  constructor(private createAcconunt: CreateAccountService, private fb: FormBuilder) {
 
-  
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    })
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+  criarUser() {
+
+    this.createAcconunt.createUser(this.userForm.value).subscribe(
+      response => {
+      
+      },
+      error => {
+        console.error('Erro ao criar usuário:', error);
+      }
+    )
+  }
+
+
 }
